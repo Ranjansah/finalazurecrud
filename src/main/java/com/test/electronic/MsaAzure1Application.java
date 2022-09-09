1797a109e8f8fe13c2f79eb014c93c27fc76387e
Ranjan <sahaadi99@gmail.com>
2022-09-09 16:54:20 +0530

    done first commit

---
+++ src/main/java/com/test/electronic/MsaAzure1Application.java
@@ -0,0 +1,13 @@
+package com.test.electronic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaAzure1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaAzure1Application.class, args);
+	}
+
+}
